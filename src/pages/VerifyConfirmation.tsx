d7f87d57616ff58983e7a9c2e3a769096744bfdd
JoeMei0603 <joel.meier4@uzh.ch>
2022-05-27 10:41:18 +0200

    Minor grammatical changes to verify page

--- src/pages/VerifyConfirmation.tsx
+++ src/pages/VerifyConfirmation.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Container, Heading, VStack} from '@chakra-ui/react'
+import {Container, Heading, VStack, Text} from '@chakra-ui/react'
 import {Icon} from "@chakra-ui/icons";
 import {ReactComponent as CheckmarkIllustration} from "../assets/checkmark.svg";
 
@@ -8,11 +8,11 @@ export default function VerifyConfirmation() {
         <VStack minH='100vh' minW='fit-content' p={4} spacing={12} position='relative' justify='center'>
             <VStack>
                 <Heading>Thank you for registering on group matcher!</Heading>
+                <Text textAlign='center' w='md' color='gray.600'>Your account has been successfully created.</Text>
             </VStack>
             <Container gap={6} w='lg' centerContent bg='white' boxShadow='lg' rounded='3xl' borderWidth={1} p={10}>
                 <Icon as={CheckmarkIllustration} boxSize='10rem' />
-                <Heading fontSize='lg'>Your account still has to be confirmed.</Heading>
-                <Heading fontSize='lg'>Please check your email inbox!</Heading>
+                <Heading fontSize='lg'>Before you can proceed, please verify your email.</Heading>
             </Container>
         </VStack>
     )
